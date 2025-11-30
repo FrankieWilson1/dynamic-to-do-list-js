5e2bd2f5190245db25e9e1658d6373da7f5f6896
frankiewilson1 <frankwilliamsugwu@gmail.com>
2025-11-30 11:55:54 -0800

    feature: implement local storage functionality

--- script.js
+++ script.js
@@ -5,6 +5,50 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
+    // Call loadtask
+    loadTasks();
+
+    // Initialize and Load Tasks
+    function loadTasks() {
+        // Retrive the JSON string from localStorage
+        const storedTasks = localStorage.getItem('task');
+
+        if (storedTasks) {
+            const saveTasks = JSON.parse(storedTasks);
+
+            saveTasks.forEach(taskText => {
+                const liElement = document.createElement('li');
+                liElement.innerHTML = taskText;
+
+                // Create "Remove" Button
+                const removeBtn = document.createElement('button');
+                removeBtn.innerHTML = 'Remove';
+                removeBtn.classList.add("remove-btn");
+
+                // Update event listener to als update localstorage
+                removeBtn.addEventListener("click", () => {
+                    liElement.remove();
+                    updateLocalStorage();
+                });
+
+                // Append to DOM
+                liElement.appendChild(removeBtn);
+                taskList.append(liElement);
+            });
+        }
+    }
+
+    // Update local function to remove or add task
+    function updateLocalStorage() {
+        const allLiElements = taskList.querySelectorAll('li');
+        const currentTaskArray = [...allLiElements].map(li => {
+            return li.firstChild.textContent.trim();
+        });
+        const jsonData = JSON.stringify(currentTaskArray);
+        localStorage.setItem('task', jsonData);
+        
+    }
+
     // addTask function that will be called to add new tasks to the list.
     function addTask() {
         // Retrieve and trim the value from the task input field.
@@ -29,6 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Assign an "onclick" event to the remove button that when triggered, removes button to the li element, then append the the "li" element from "taskList".
             removeBtn.addEventListener("click", () => {
                 liElement.remove();
+                updateLocalStorage();
             });
 
             // Append the remove button to the li element, then append the li to taskList.
@@ -38,6 +83,9 @@ document.addEventListener('DOMContentLoaded', () => {
             // Clear the task input field
             taskInput.value = '';
         }
+
+        // save task
+        updateLocalStorage();
     }
 
     // Event listener that calls "addTask" when the button is clicked.
